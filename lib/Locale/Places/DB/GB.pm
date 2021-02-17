3a7275092ddcbc8537a8b9642349562d98afb9ea
Nigel Horne <nigel.horne@nasa.gov>
2021-02-17 14:12:16 -0500

    Fix column name

--- lib/Locale/Places/DB/GB.pm
+++ lib/Locale/Places/DB/GB.pm
@@ -25,7 +25,7 @@ our @ISA = ('Locale::Places::DB');
 sub _open {
 	my $self = shift;
 
-	return $self->SUPER::_open(sep_char => "\t", column_names => ['code1','code2','type','data','is_preferred_name']);
+	return $self->SUPER::_open(sep_char => "\t", column_names => ['code1','code2','type','data','ispreferredname']);
 }
 
 1;
