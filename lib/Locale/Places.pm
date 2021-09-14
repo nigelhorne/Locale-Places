a2220985df1f90240e04f66bab129ed9eb53262c
Nigel Horne <nigel.horne@nasa.gov>
2021-09-14 13:52:08 -0400

    Release 0.06

--- lib/Locale/Places.pm
+++ lib/Locale/Places.pm
@@ -15,11 +15,11 @@ Locale::Places - Translate places using http://download.geonames.org/
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 METHODS
 
