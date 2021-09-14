3c54a7ae990891d72eb231fae04ad3795dd83ee5
Nigel Horne <nigel.horne@nasa.gov>
2021-09-14 13:05:20 -0400

    Fix CI building

--- lib/Locale/Places.pm
+++ lib/Locale/Places.pm
@@ -137,7 +137,7 @@ sub translate {
 			return $data;
 		}
 	} elsif(scalar(@places) > 1) {
-		@places = $self->{'gb'}->code2({ type => $from, data => $place, ispreferredname => 1, isshortname => 0 });
+		@places = $self->{'gb'}->code2({ type => $from, data => $place, ispreferredname => 1, isshortname => undef });
 		if(scalar(@places) == 1) {
 			if(my $data = $self->{'gb'}->data({ type => $to, code2 => $places[0] })) {
 				return $data;
@@ -151,9 +151,11 @@ sub translate {
 				return $place;
 			}
 		} elsif(scalar(@places) == 0) {
-			@places = $self->{'gb'}->code2({ type => $from, data => $place, isshortname => 0 });
-			if(my $data = $self->{'gb'}->data({ type => $to, code2 => $places[0] })) {
-				return $data;
+			@places = $self->{'gb'}->code2({ type => $from, data => $place, isshortname => undef });
+			if(scalar(@places) == 1) {
+				if(my $data = $self->{'gb'}->data({ type => $to, code2 => $places[0] })) {
+					return $data;
+				}
 			}
 		}
 		Carp::croak(__PACKAGE__, ": database has more than one preferred entry for $place in language $to");
