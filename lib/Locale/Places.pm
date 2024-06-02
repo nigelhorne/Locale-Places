b9619f1b5127e8c1b0f1944e8cc00fb3fff1230c
Nigel Horne <njh@bandsman.co.uk>
2024-06-01 20:48:06 -0400

    Release 0.12

--- lib/Locale/Places.pm
+++ lib/Locale/Places.pm
@@ -21,11 +21,11 @@ Locale::Places - Translate places between different languages using http://downl
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
